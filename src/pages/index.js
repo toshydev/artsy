cde27474cae99b435a5a44e9990b53b8567f797a
imac <frank.siyli@gmail.com>
2023-03-21 17:26:33 +0100

    add spotlight page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
+import Spotlight from "@/components/Spotlight";
+
+
+
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <Spotlight/>
     </div>
   );
 }
