e2dd33d1e0843dd51d18fdd7e85827439ef9a579
Anton Roters <antonroters@hotmail.com>
2023-03-21 18:16:03 +0100

    move data fetching to _app, US-3 finished

--- src/components/Spotlight/index.js
+++ src/components/Spotlight/index.js
@@ -1,29 +1,17 @@
 import Image from "next/image";
-import useSWR from "swr"
-import React from 'react';
 
+export default function Spotlight({ pieces }) {
+  const currentPiece = pieces[Math.floor(Math.random() * pieces.length)];
 
-const fetcher = (...args) => fetch(...args).then(res => res.json())
-
-
-
-export default function Spotlight () {
-    const {data, error, isLoading} = useSWR("https://example-apis.vercel.app/api/art", fetcher)
-
-
-    if (error) return <div>Failed to fetch users.</div>
-    if (isLoading) return <h2>Loading...</h2>
-    
-    const currentArtist = data[Math.floor(Math.random() * data.length)]
-
-    return (<>   
-                    
-        <Image src={currentArtist.imageSource}
-               alt={currentArtist.name}
-               height={400}
-                width={300}
-               ></Image> 
-               <h3 key= {Math.random()}>Spotlight: @{currentArtist.name}</h3>     
-              </>
-      )
-  }
+  return (
+    <>
+      <Image
+        src={currentPiece.imageSource}
+        alt={currentPiece.name}
+        height={400}
+        width={300}
+      ></Image>
+      <h3 key={currentPiece.slug}>Spotlight: @{currentPiece.name}</h3>
+    </>
+  );
+}
