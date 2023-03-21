043574c04945b903b33b614ecb96a3d0fa7df2a3
Stephan Niendorf <stephan@stephanmitph.com>
2023-03-21 18:42:59 +0100

    remove code in index.js

--- src/pages/art-pieces/[slug].js
+++ src/pages/art-pieces/[slug].js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { useRouter } from "next/router";
+//import { useRouter } from "next/router";
 
 export default function ArtPieceDetails({imageSource, name, artist, year, genre, dimensions}) {
         //const router = useRouter();
--- src/pages/art-pieces/index.js
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-
-
-export default function ArtPieceDetail () {
-    const router = useRouter();
-    const { slug } = router.query;
-    const currentArtPiece = artPieces.indexOf(currentArtPiece);
-}
