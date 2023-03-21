37c61c8c492f09a6cab8684bd1fa702964133c0a
Stephan Niendorf <stephan@stephanmitph.com>
2023-03-21 17:38:43 +0100

    remove empty function

--- src/pages/art-pieces/index.js
+++ src/pages/art-pieces/index.js
@@ -5,11 +5,4 @@ export default function ArtPieceDetail () {
     const router = useRouter();
     const { slug } = router.query;
     const currentArtPiece = artPieces.indexOf(currentArtPiece);
-
-    if (!currentArtPiece) {
-        return;
-    }
-    return (
-        <
-    )
 }
