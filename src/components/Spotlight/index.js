a076c7cc5da47b203f53c90500cdb87d7c3a09f2
Malgorzata Wawrzyniak <malgosia.archi@gmail.com>
2023-03-22 15:23:05 +0100

    add FavouriteButton

--- src/components/Spotlight/index.js
+++ src/components/Spotlight/index.js
@@ -1,8 +1,15 @@
 import Image from "next/image";
+import FavouriteButton from "../FavouriteButton";
 
-
-export default function Spotlight({ pieces }) {
+export default function Spotlight({
+  pieces,
+  onToggleFavourite,
+  artPiecesInfo,
+}) {
   const currentPiece = pieces[Math.floor(Math.random() * pieces.length)];
+  /* const isFavourite = artPiecesInfo.find((piece) => {
+    piece.slug === currentPiece.slug;
+  })?.isFavourite; */
 
   return (
     <>
@@ -11,9 +18,13 @@ export default function Spotlight({ pieces }) {
         alt={currentPiece.name}
         height={400}
         width={300}
-      ></Image>
+      />
+      <FavouriteButton
+        onToggleFavourite={onToggleFavourite}
+        slug={currentPiece.slug}
+        artPiecesInfo={artPiecesInfo}
+      />
       <h3 key={currentPiece.slug}>Spotlight: @{currentPiece.artist}</h3>
     </>
   );
 }
-
